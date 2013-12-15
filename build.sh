7ec46e56235bd2aa05cb4d607d3e1aeffc509ec3
Ilya Zverev <zverik@textual.ru>
2013-12-15 14:39:50 +0400

    fix path in mapbbcode-window

--- build.sh
+++ build.sh
@@ -8,6 +8,9 @@ rm $DISTFILE
 mkdir files
 mv mapbbcode files
 wget -nv https://raw.github.com/MapBBCode/mapbbcode-loader/master/MapBBCodeLoader.min.js -O files/mapbbcode/MapBBCodeLoader.min.js
+wget -nv https://raw.github.com/MapBBCode/mapbbcode-loader/master/mapbbcode-window.html -O files/mapbbcode/mapbbcode-window.html
+# this is awesome line editing to fix mapbbcode path
+printf '/force/\n-a\npath: ".",\n.\nw\n' | ed files/mapbbcode/mapbbcode-window.html
 
 rm -f $TARGET
 zip -qr $TARGET readme.txt files
